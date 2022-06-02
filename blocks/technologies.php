9d2a17ed4643355f85f125426b3d0e314730a964
radostBeZuma <romahasasha@yandex.ru>
2022-06-02 14:59:26 +0300

    добавление блока technologies

---
+++ blocks/technologies.php
@@ -0,0 +1,42 @@
+<section class="technologies">
+	<div class="wrapper technologies__wrap">
+		<div class="technologies__inner">
+			<h2 class="technologies__title">Technologies</h2>
+			<p class="technologies__desc">
+				<span class="technologies__desc technologies__desc--text">Stan's</span>
+				<span class="technologies__desc technologies__desc--text">Assets</span>
+				is a team of developers that are committed to developing high quality and engaging entertainment software
+			</p>
+			<div class="technologies__container-softs">
+				<div class="technologies__box-soft">
+					<img class="technologies__img-soft" src="/uploads/img/android.png" alt="Soft android">
+				</div>
+				<div class="technologies__box-soft">
+					<img class="technologies__img-soft" src="/uploads/img/ios.png" alt="Soft android">
+				</div>
+				<div class="technologies__box-soft">
+					<img class="technologies__img-soft" src="/uploads/img/td.png" alt="Soft android">
+				</div>
+			</div>
+			<p class="technologies__soft-desc">Our engineers develop applications for the most popular platforms.</p>
+			<!-- <div class="technologies__container-clutch">
+				<img class="technologies__clutch-logo" src="/uploads/img/clutch.png" alt="">
+				<div class="technologies__box-reviews">
+					<p class="technologies__mark-reviews">4.5</p>
+					<div class="technologies__box-stars">
+						<img class="technologies__img-star" src="/uploads/img/star.png" alt="">
+						<img class="technologies__img-star" src="/uploads/img/star.png" alt="">
+						<img class="technologies__img-star" src="/uploads/img/star.png" alt="">
+						<img class="technologies__img-star" src="/uploads/img/star.png" alt="">
+						<img class="technologies__img-star" src="/uploads/img/star.png" alt="">
+					</div>
+					<p class="technologies__count-reviews">
+						<span class="technologies__count">2</span>
+						<span class="technologies__count-reviews-text">reviews</span>
+					</p>
+				</div>
+				<button class="technologies__btn-clutch" type="button">Check out reviews</button>
+			</div> -->
+		</div>
+	</div>
+</section>
--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -6,25 +6,34 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<!-- all-page css init -->
-	<link rel="stylesheet" href="/css/entry/fonts.css">
-	<link rel="stylesheet" href="/css/entry/reset-style.css">
-	<link rel="stylesheet" href="/css/entry/components.css">
-	<link rel="stylesheet" href="/css/entry/variables.css">
+		<link rel="stylesheet" href="/css/entry/fonts.css">
+		<link rel="stylesheet" href="/css/entry/reset-style.css">
+		<link rel="stylesheet" href="/css/entry/components.css">
+		<link rel="stylesheet" href="/css/entry/variables.css">
 	<!-- all-page css init end -->
 
-	<link rel="stylesheet" href="/css/blocks/header.css">
-	<link rel="stylesheet" href="/css/blocks/top-block.css">
-	<link rel="stylesheet" href="/css/blocks/social-links.css">
+	<!-- index-tpl css -->
+		<link rel="stylesheet" href="/css/pages/index-page.css">
+	<!-- index-tpl css end -->
+
+	<!-- index-page css init -->
+		<link rel="stylesheet" href="/css/blocks/header.css">
+		<link rel="stylesheet" href="/css/blocks/top-block.css">
+		<link rel="stylesheet" href="/css/blocks/social-links.css">
+		<link rel="stylesheet" href="/css/blocks/technologies.css">
+	<!-- index-page css init end -->
+
+
 	<title>Stan`s assets</title>
 </head>
 <body>
 	<? require ('../blocks/header.php') ?>
 	<? require ('../blocks/top-block.php') ?>
 	<? require ('../blocks/social-links.php') ?>
+	<? require ('../blocks/technologies.php') ?>
+	
+	<!-- <p style="height: 800px">content</p> -->
 
-	<p style="height: 800px">content</p>
-
-	<!-- <h1>FOCUS ON THE PRODUCT AND LET US DO THE REST!</h1> -->
 	
 </body>
 </html>
